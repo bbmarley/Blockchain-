4b1869cd81e21fbda85a1ed4304e4d010515b2d8
BKumar <43064317+bbmarley@users.noreply.github.com>
2021-03-23 12:11:54 +0530

    Update ethertransfer.js

--- erc20token transfer/ethertransfer.js
+++ erc20token transfer/ethertransfer.js
@@ -3,15 +3,15 @@ var Tx = require('ethereumjs-tx');
 var Web3Utils = require('web3-utils');
 // Show Web3 where it needs to look for a connection to Ethereum.
 //web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
-const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/690b1a5f315d4feeb65bf406ac059c15'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/9b1a5f315d4feeb65bf406ac0515'));
 // var gasPrice = "5";//or get with web3.eth.gasPrice
 // var gasPrice = 5;
     // web3.eth.getGasPrice(function(error, result) {
     //         console.log('Gas Price: '+ result);
     //         gasPrice = result;
 // var gasLimit = 21000;
-var addr = "0x8477ce2B3d635C8Af4B5D00a8a4Bf0cFEe4D8951";
-var toAddress = "0xE0E223D0c1828795B95a672B2E0AFc586964f05c";
+var addr = "0x8477ce2B3d63fa4Bf0cFEe4D8951";
+var toAddress = "0xE0E221828795B95a6B2E0AFc586964f05c";
 // var amountToSend = "0.1"; //$1
 var nonce = web3.eth.getTransactionCount(addr);
 var rawTransaction = {
@@ -22,7 +22,7 @@ var rawTransaction = {
 "to": toAddress,
 "value": web3.toHex(Web3Utils.toWei('1', "ether")),
 };
-var privateKey = "273A9DFBA15B2730A05DBB8B05445C55309B7CB761595BEA4C7D91EE2B85804F";
+var privateKey = "2A9DFBA15B2730A05DBB8B05455309B7CB761595BD91EE2B85804F";
 var privKey = new Buffer(privateKey, 'hex');
 console.log("privKey  : ", privKey);
 var tx = new Tx(rawTransaction);
